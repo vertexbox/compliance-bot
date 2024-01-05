892013ba05f8d82eafce85d87bf4956462ffe2e0
sumire <sumire88@proton.me>
2024-01-06 00:08:22 +0800

    feat: bootstrap event handler

---
+++ src/common.ts
@@ -0,0 +1,34 @@
+import { Probot, ProbotOctokit, Context } from "probot";
+
+export { Handler, HandlerModule, Repository, Extension, Result };
+
+interface Handler {
+  (
+    event: string,
+    context: Context<any>,
+    app: Probot,
+    repo: Repository,
+    extension: Extension,
+  ): Promise<Result>;
+}
+
+interface HandlerModule {
+  name: string;
+  event_triggers: string[];
+  description: string;
+  handler: Handler;
+}
+
+type Extension = {
+  octokit: InstanceType<typeof ProbotOctokit>;
+};
+
+type Repository = {
+  owner: string;
+  name: string;
+};
+
+type Result = {
+  result: string;
+  error?: string;
+};
---
+++ src/constant.ts
@@ -0,0 +1,19 @@
+export const defaultLables = [
+  "fix",
+  "feat",
+  "feature",
+  "patch",
+  "hotfix",
+  "ci",
+  "optimize",
+  "refactor",
+  "style",
+  "doc",
+  "docs",
+  "mock",
+  "fixture",
+  "chore",
+  "test",
+];
+
+export const strictLabels = defaultLables.slice(0, -5);
