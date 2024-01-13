0e7373bacc882e19582caac54418d4db1a769b3c
kev <yqlbu@bu.edu>
2024-01-13 19:29:53 +0800

    feat: add check_pr_documentation_label handler

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { HandlerModule } from "./common";
 import ValidatePRTileHandler from "./handlers/validate_pr_title";
 import CheckPRTestedLabel from "./handlers/check_pr_tested_label";
+import CheckPRDocumentationLabel from "./handlers/check_pr_documentation_label";
 
 export interface Configuration {
   app_name: string;
@@ -9,6 +10,7 @@ export interface Configuration {
 export const HandlerModules: HandlerModule[] = [
   ValidatePRTileHandler,
   CheckPRTestedLabel,
+  CheckPRDocumentationLabel,
 ];
 
 export const AppConfig: Configuration = {
